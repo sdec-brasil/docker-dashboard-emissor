21605014b95e4e01626b371dcd23e8bf90df6efd
David Beyda <dbeyda@globo.com>
2019-09-09 11:05:15 -0300

    Initial

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo Setting up inovice-explorer...
+cd ..
+git clone https://github.com/sdec-brasil/invoice-explorer.git
+echo Done.
+
+echo Setting up docker-multichain...
+git clone https://github.com/sdec-brasil/docker-multichain.git
+echo Done.
